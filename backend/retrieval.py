6898e51f11d9d9c2c24621afc5e322dccc696313
Doid888 <6631501074@lamduan.mfu.ac.th>
2026-04-11 22:35:47 +0700

    Semantic Search has been successfully created.

--- backend/retrieval.py
+++ backend/retrieval.py
@@ -1,6 +1,7 @@
 from qdrant_client import QdrantClient
 from dotenv import load_dotenv
 import os
+from sentence_transformers import SentenceTransformer
 
 load_dotenv()
 
@@ -8,5 +9,20 @@ client = QdrantClient(
     url=os.getenv("QDRANT_URL"),
     api_key=os.getenv("QDRANT_API_KEY"),
 )
-
 print(client.get_collections())
+
+model = SentenceTransformer("intfloat/multilingual-e5-base")
+
+query = "Methology of the Petfeeder project"
+
+query_vector = model.encode([query])[0]
+
+results = client.query_points(
+    collection_name="project_documents",  # 👈 ใช้ชื่อที่มีจริง
+    query=query_vector,
+    limit=3
+)
+
+context = [point.payload["text"] for point in results.points]
+
+print(context)
