c735027f91b34e99464795d0fb87fa9053bedb38
FRAmyZ010 <pongsapat.pp2004@gmail.com>
2026-08-21 00:54:35 +0700

    Complete config.py

---
+++ backend/app/core/config.py
@@ -0,0 +1,34 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Optional
+
+class Settings(BaseSettings):
+
+    # App Settings
+    APP_NAME: str = "RAGcoon Backend"
+    DEBUG: bool = True
+
+    # PostgreSQL Config
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST:str = "postgres"
+    POSTGRES_PORT: int = 5432
+    DATABASE_URL: Optional[str] = None
+
+    # Qdrant Cloud Config
+    QDRANT_URL: str
+    QDRANT_API_KEY: str
+
+    model_config = SettingsConfigDict(
+        env_file = ".env",
+        env_file_encoding = "utf-8",
+        extra = "ignore"
+    )
+
+    def get_database_url(self) -> str:
+        if self.DATABASE_URL:
+            return self.DATABASE_URL
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+
+settings = Settings()   
